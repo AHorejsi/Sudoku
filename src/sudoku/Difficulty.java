98c87f5170567c8fc85c0a7ed1fc07d7195abfff
AHorejsi <alex.horejsi59@gmail.com>
2018-05-08 01:36:46 -0700

    Redesigned how puzzles will be generated

--- src/sudoku/Difficulty.java
+++ src/sudoku/Difficulty.java
@@ -1,6 +1,33 @@
 package sudoku;
 
+import java.util.Random;
+
 public enum Difficulty {
-	EASY,
-	HARD;
+	EASY (0.44f, 0.57f, 0.56f),
+	HARD (0.35f, 0.38f, 0.22f);
+	
+	private float minPercent;
+	private float maxPercent;
+	private float lowerBound;
+	
+	private Difficulty(float minPercent, float maxPercent, float lowerBound) {
+		this.minPercent = minPercent;
+		this.maxPercent = maxPercent;
+	}
+	
+	public float getMinPercent() {
+		return this.minPercent;
+	}
+	
+	public float getMaxPercent() {
+		return this.maxPercent;
+	}
+	
+	public float getLowerBound() {
+		return this.lowerBound;
+	}
+	
+	public double getRandomPercent(Random rng) {
+		return this.minPercent + (this.maxPercent - this.minPercent) * rng.nextFloat();
+	}
 }
--- src/sudoku/Dimensions.java
+++ src/sudoku/Dimensions.java
@@ -1,6 +1,22 @@
 package sudoku;
 
 public enum Dimensions {
-	NINE_BY_NINE,
-	SIXTEEN_BY_SIXTEEN;
+	NINE_BY_NINE (9, 81),
+	SIXTEEN_BY_SIXTEEN (16, 256);
+	
+	private int totalNumberOfSpaces;
+	private int perBoxRowColumn;
+	
+	private Dimensions(int perBoxRowColumn, int total) {
+		this.totalNumberOfSpaces = total;
+		this.perBoxRowColumn = perBoxRowColumn;
+	}
+	
+	public int getTotalNumberOfSpaces() {
+		return this.totalNumberOfSpaces;
+	}
+	
+	public int getPerBoxRowColumn() {
+		return this.perBoxRowColumn;
+	}
 }
