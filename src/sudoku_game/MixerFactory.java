9589e972c3cc40b9116c9e3288e4a8661dd399b7
AHorejsi <alex.horejsi59@gmail.com>
2018-06-11 01:59:30 -0700

    Wrote documentation for MixerFactories

--- src/sudoku_game/MixerFactory.java
+++ src/sudoku_game/MixerFactory.java
@@ -2,7 +2,20 @@ package sudoku_game;
 
 import java.util.Collection;
 
+/**
+ * A factory that produces a {@code Collection} consisting
+ * of one {@code Mixer} or more
+ * @author Alex Horejsi
+ */
 @FunctionalInterface
 public interface MixerFactory {
+	/**
+	 * Creates a {@code Mixer} {@code Collection}
+	 * based on the given info
+	 * @param info The info determining the contents
+	 * of the {@code Collection}
+	 * @return A {@code Collection} consisting
+	 * of one {@code Mixer} or more
+	 */
 	public Collection<Mixer> getFactory(String info);
 }
--- src/sudoku_game/SimpleMixerFactory.java
+++ src/sudoku_game/SimpleMixerFactory.java
@@ -9,11 +9,97 @@ import java.util.Vector;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.CopyOnWriteArraySet;
 
+/**
+ * This implementation of {@code MixerFactory} allows
+ * a user to choose what implementations of {@code Mixer}
+ * to use and what kind of data structure to put them in.
+ * The input {@code String} should consist of several words,
+ * separated by whitespace characters. The first word should be the
+ * type of data structure to be used. The following words
+ * should indicate the implementations of {@code Mixer} to
+ * use
+ * <br><br>
+ * Available data structures:
+ * <table>
+ * 	<thead>
+ * 		<tr>
+ * 			<th>Class</th>
+ * 			<th>Input String</th>
+ * 		</tr>
+ * 	</thead>
+ * 	<tbody>
+ * 		<tr>
+ * 			<td>{@code ArrayList}</td>
+ * 			<td>"arraylist"</td>
+ * 		</tr>
+ * 		<tr>
+ * 			<td>{@code LinkedList}</td>
+ * 			<td>"linkedlist"</td>
+ * 		</tr>
+ * 		<tr>
+ * 			<td>{@code Vector}</td>
+ * 			<td>"vector"</td>
+ * 		</tr>
+ * 		<tr>
+ * 			<td>{@code CopyOnWriteArrayList}</td>
+ * 			<td>"copyonwritearraylist"</td>
+ * 		</tr>
+ * 		<tr>
+ * 			<td>{@code HashSet}</td>
+ * 			<td>"hashset"</td>
+ * 		</tr>
+ * 		<tr>
+ * 			<td>{@code LinkedHashSet}</td>
+ * 			<td>"linkedhashset"</td>
+ * 		</tr>
+ * 		<tr>
+ * 			<td>{@code CopyOnWriteArraySet}</td>
+ * 			<td>"copyonwritearrayset"</td>
+ * 		</tr>
+ * 	</tbody>
+ * </table>
+ * 
+ * <br>
+ * Available implementations of {@code Mixer}:
+ * <table>
+ * 	<thead>
+ * 		<tr>
+ * 			<th>Class</th>
+ * 			<th>Input String</th>
+ * 		</tr>
+ * 	</thead>
+ * 	<tbody>
+ * 		<tr>
+ * 			<td>{@code RotateMixer}</td>
+ * 			<td>"rotate"</td>
+ * 		</tr>
+ * 		<tr>
+ * 			<td>{@code FlipMixer}</td>
+ * 			<td>"flip"</td>
+ * 		</tr>
+ * 		<tr>
+ * 			<td>{@code FlipBoxMixer}</td>
+ * 			<td>"flipbox"</td>
+ * 		</tr>
+ * 		<tr>
+ * 			<td>{@code SwapMixer}</td>
+ * 			<td>"swap"</td>
+ * 		</tr>
+ * 	</tbody>
+ * </table>
+ * @author Alex Horejsi
+ */
 public class SimpleMixerFactory implements MixerFactory {
 	private static MixerFactory factory = new SimpleMixerFactory();
 	
 	private SimpleMixerFactory() {}
 	
+	/**
+	 * Returns the single instance
+	 * of {@code SimpleMixerFactory}
+	 * @return The single instance
+	 * of {@code SimpleMixerFactory}
+	 */
 	public static MixerFactory getInstance() {
 		return SimpleMixerFactory.factory;
 	}
