64a52b2553bcb6344a2c7c288992cad9eaf3181d
AHorejsi <alex.horejsi59@gmail.com>
2018-06-29 05:59:17 -0700

    Finished implementations of LegalValues.java

--- src/sudoku_game/LegalValues.java
+++ src/sudoku_game/LegalValues.java
@@ -1,5 +1,7 @@
 package sudoku_game;
 
+import java.util.Arrays;
+
 /**
  * Classes that implement this interface
  * are meant to define the <tt>char</tt>
@@ -7,7 +9,13 @@ package sudoku_game;
  * a Sudoku puzzle with certain dimensions
  * @author Alex Horejsi
  */
-interface LegalValues {
+abstract class LegalValues {
+	private char[] values;
+	
+	protected LegalValues(char[] values) {
+		this.values = values;
+	}
+	
 	/**
 	 * Checks if the given <tt>char</tt>
 	 * value is legal for this Sudoku
@@ -18,15 +26,21 @@ interface LegalValues {
 	 * <tt>char</tt> value is legal for a
 	 * Sudoku puzzle
 	 */
-	public boolean isLegal(char value);
+	public boolean isLegal(char value) {
+		return Arrays.binarySearch(this.values, value) >= 0;
+	}
 	
 	/**
 	 * Returns a <tt>char</tt> array
 	 * containing all of the legal <tt>char</tt>
-	 * values for this Sudoku puzzle
+	 * values for this Sudoku puzzle. The <tt>char</tt>
+	 * array that is returned should be a independent
+	 * of any data fields in this class
 	 * @return A <tt>char</tt> array containing
 	 * all of the legal <tt>char</tt> values for
 	 * a Sudoku puzzle
 	 */
-	public char[] getLegalValues();
+	public char[] getLegalValues() {
+		return Arrays.copyOf(this.values, this.values.length);
+	}
 }
--- src/sudoku_game/LegalValues9x9.java
+++ src/sudoku_game/LegalValues9x9.java
@@ -1,18 +1,17 @@
 package sudoku_game;
 
-import java.util.Arrays;
-
 /**
  * This class defines the legal <tt>char</tt>
  * values for nine-by-nine Sudoku puzzles.<br>
  * Legal values: 1, 2, 3, 4, 5, 6, 7, 8, 9
  * @author Alex Horejsi
  */
-class LegalValues9x9 implements LegalValues {
+class LegalValues9x9 extends LegalValues {
 	private static LegalValues val = new LegalValues9x9();
-	private char[] values = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
 	
-	private LegalValues9x9() {}
+	private LegalValues9x9() {
+		super(new char[] {'1', '2', '3', '4', '5', '6', '7', '8', '9'});
+	}
 	
 	/**
 	 * Returns the single instance of
@@ -26,11 +25,6 @@ class LegalValues9x9 implements LegalValues {
 	
 	@Override
 	public boolean isLegal(char value) {
-		return Arrays.binarySearch(this.values, value) >= 0;
-	}
-	
-	@Override
-	public char[] getLegalValues() {
-		return this.values;
+		return value >= '1' && value <= '9';
 	}
 }
