df6fa1f0fc535de897d72caa53225408ae20971e
AHorejsi <alex.horejsi59@gmail.com>
2018-07-02 09:39:27 -0700

    Added code to Generator4x4.java that fixed a bug

--- src/sudoku_game/Generator4x4.java
+++ src/sudoku_game/Generator4x4.java
@@ -82,16 +82,59 @@ class Generator4x4 implements Generator {
 		}
 		
 		for (char digit = '1' ; digit <= '4' ; digit++) {
-			if (this.table[i][j] == null)
-				this.table[i][j] = new ConcreteCell(digit);
-			else
-				this.table[i][j].setValueForSetUp(digit);
-				
-			if (this.fillRemaining(i, j + 1))
-				return true;
-			this.table[i][j].setEmptyForSetUp();
+			if (this.safe(i, j, digit)) {
+				if (this.table[i][j] == null)
+					this.table[i][j] = new ConcreteCell(digit);
+				else
+					this.table[i][j].setValueForSetUp(digit);
+					
+				if (this.fillRemaining(i, j + 1))
+					return true;
+				this.table[i][j].setEmptyForSetUp();
+			}
 		}
 		
 		return false;
+	}
+	
+	private boolean safe(int i, int j, char digit) {
+		return this.safeRow(i, digit) && 
+			   this.safeCol(j, digit) && 
+			   this.safeBox(i - i % 2, j - j % 2, digit);
+	}
+	
+	private boolean safeRow(int i, char digit) {
+		for (int j = 0 ; j < 4 ; j++) {
+			if (this.table[i][j] != null) {
+				if (this.table[i][j].getValue() == digit)
+					return false;
+			}
+		}
+		
+		return true;
+	}
+	
+	private boolean safeCol(int j, char digit) {
+		for (int i = 0 ; i < 4 ; i++) {
+			if (this.table[i][j] != null) {
+				if (this.table[i][j].getValue() == digit)
+					return false;
+			}
+		}
+		
+		return true;
+	}
+	
+	private boolean safeBox(int i, int j, char digit) {
+		for (int row = 0 ; row < 2 ; row++) {
+			for (int col = j ; col < 3 ; col++) {
+				if (this.table[row + i][col + j] != null) {
+					if (this.table[row + i][col + j].getValue() == digit)
+						return false;
+				}
+			}
+		}
+		
+		return true;
 	}
 }
