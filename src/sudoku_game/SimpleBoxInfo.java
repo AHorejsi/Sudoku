68737662298655ecc451a5c0f822a66e45b71cec
AHorejsi <alex.horejsi59@gmail.com>
2018-07-15 03:22:34 -0700

    Added documentation for SimpleBoxInfo.java

--- src/sudoku_game/SimpleBoxInfo.java
+++ src/sudoku_game/SimpleBoxInfo.java
@@ -1,9 +1,22 @@
 package sudoku_game;
 
+/**
+ * Simple implementation
+ * of {@code BoxInfo}
+ * @author Alex Horejsi
+ */
 public class SimpleBoxInfo implements BoxInfo {
 	private int rows;
 	private int cols;
 	
+	/**
+	 * Defines the row and column
+	 * sizes for this {@code BoxInfo}
+	 * @param rowSize The number of
+	 * rows for the boxes
+	 * @param colSize The number of
+	 * columns for the boxes
+	 */
 	public SimpleBoxInfo(int rowSize, int colSize) {
 		this.rows = rowSize;
 		this.cols = colSize;
