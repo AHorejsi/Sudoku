b7916e1a5c6d8365e0ec45e85ae1c99264f623f3
AHorejsi <alex.horejsi59@gmail.com>
2018-07-23 08:50:33 -0700

    Added documentation to AllMixer.java

--- src/sudoku_game/AllMixer.java
+++ src/sudoku_game/AllMixer.java
@@ -2,11 +2,23 @@ package sudoku_game;
 
 import java.util.Random;
 
+/**
+ * This class runs all instances of
+ * {@code Mixer} on the given Sudoku
+ * puzzle
+ * @author Alex Horejsi
+ */
 public class AllMixer implements Mixer {
 	private static Mixer mixer = new AllMixer();
 	
 	private AllMixer() {}
 	
+	/**
+	 * Returns the single instance
+	 * of {@code AllMixer}
+	 * @return The single instance
+	 * of {@code AllMixer}
+	 */
 	public static Mixer getInstance() {
 		return AllMixer.mixer;
 	}
