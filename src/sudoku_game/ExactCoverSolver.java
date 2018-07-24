8c071c377dd81582ce056fa8b257ac892f6e3941
AHorejsi <alex.horejsi59@gmail.com>
2018-07-23 20:18:04 -0700

    Progress in solvers

--- src/sudoku_game/ExactCoverSolver.java
+++ src/sudoku_game/ExactCoverSolver.java
@@ -1,7 +1,16 @@
 package sudoku_game;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 public class ExactCoverSolver implements Solver {
 	private static Solver solver = new ExactCoverSolver();
+	private Board board;
+	private char[] values;
+	private ColumnNode header;
+	private List<Node> answer;
 	
 	private ExactCoverSolver() {}
 	
@@ -9,280 +18,275 @@ public class ExactCoverSolver implements Solver {
 		return ExactCoverSolver.solver;
 	}
 	
-//	private boolean[][] createMatrix() {
-//	boolean[][] matrix = new boolean[4096][1024];
-//	
-//	int hBase = 0;
-//	hBase = this.checkCellConstraint(hBase, matrix);
-//	hBase = this.checkRowConstraint(hBase, matrix);
-//	hBase = this.checkColumnConstraint(hBase, matrix);
-//	this.checkBoxConstraint(hBase, matrix);
-//	
-//	return matrix;
-//}
-//
-//private int checkCellConstraint(int hBase, boolean[][] matrix) {
-//	for (int row = 0 ; row < 16 ; row++) {
-//		for (int column = 0 ; column < 16 ; column++, hBase++) {
-//			for (int valuesIndex = 0 ; valuesIndex < 16 ; valuesIndex++) {
-//				int index = this.getIndex(row, column, valuesIndex);
-//				matrix[index][hBase] = true;
-//			}
-//		}
-//	}
-//	
-//	return hBase;
-//}
-//
-//private int checkRowConstraint(int hBase, boolean[][] matrix) {
-//	for (int row = 0 ; row < 16 ; row++) {
-//		for (int valuesIndex = 0 ; valuesIndex < 16 ; valuesIndex++, hBase++) {
-//			for (int column = 0 ; column < 16 ; column++) {
-//				int index = this.getIndex(row, column, valuesIndex);
-//				matrix[index][hBase] = true;
-//			}
-//		}
-//	}
-//	
-//	return hBase;
-//}
-//
-//private int checkColumnConstraint(int hBase, boolean[][] matrix) {
-//	for (int column = 0 ; column < 16 ; column++) {
-//		for (int valuesIndex = 0 ; valuesIndex < 16 ; valuesIndex++, hBase++) {
-//			for (int row = 0 ; row < 16 ; row++) {
-//				int index = this.getIndex(row, column, valuesIndex);
-//				matrix[index][hBase] = true;
-//			}
-//		}
-//	}
-//	
-//	return hBase;
-//}
-//
-//private int getIndex(int row, int column, int valuesIndex) {
-//	return row * 16 * 16 + column * 16 + valuesIndex;
-//}
-//
-//private void checkBoxConstraint(int hBase, boolean[][] matrix) {
-//	for (int row = 0 ; row < 16 ; row += 4) {
-//		for (int column = 0 ; column < 16 ; column += 4) {
-//			for (int valuesIndex = 0 ; valuesIndex < 16 ; valuesIndex++, hBase++) {
-//				for (int rowDelta = 0 ; rowDelta < 4 ; rowDelta++) {
-//					for (int columnDelta = 0 ; columnDelta < 4 ; columnDelta++) {
-//						int index = this.getIndex(row + rowDelta, column + columnDelta, valuesIndex);
-//						matrix[index][hBase] = true;
-//					}
-//				}
-//			}
-//		}
-//	}
-//}
-//
-//private void placeInitialValues(boolean[][] matrix) {
-//	for (int i = 0 ; i < 16 ; i++) {
-//		for (int j = 0 ; j < 16 ; j++) {
-//			Cell cell = this.table[i][j];
-//			
-//			if (cell != null) {
-//				char value = cell.getValue();
-//				
-//				for (int valuesIndex = 0 ; valuesIndex < 16 ; valuesIndex++) {
-//					if (value != this.values[valuesIndex])
-//						Arrays.fill(matrix[this.getIndex(i, j, valuesIndex)], false);
-//				}
-//			}
-//		}
-//	}
-//}
-//
-//private ColumnNode createDoublyLinkedMatrix(boolean[][] matrix) {
-//	ColumnNode hNode = new ColumnNode("header");
-//	List<ColumnNode> columnNodes = new ArrayList<ColumnNode>();
-//	
-//	for (int i = 0 ; i < 1024 ; i++) {
-//		ColumnNode node = new ColumnNode(String.valueOf(i));
-//		columnNodes.add(node);
-//		hNode = (ColumnNode)hNode.hookRight(node); 
-//	}
-//	
-//	hNode = hNode.right.column;
-//	
-//	for (boolean[] row : matrix) {
-//		Node prev = null;
-//		
-//		for (int col = 0 ; col < 1024 ; col++) {
-//			if (row[col]) {
-//				ColumnNode colNode = columnNodes.get(col);
-//				Node newNode = new Node(colNode);
-//				
-//				if (prev == null)
-//					prev = newNode;
-//				
-//				colNode.up.hookDown(newNode);
-//				prev = prev.hookRight(newNode);
-//				colNode.size++;
-//			}
-//		}
-//	}
-//	
-//	hNode.size = 1024;
-//	
-//	return hNode;
-//}
-//
-//private Cell[][] search(int k, Cell[][] table) {
-//	if (this.header.right == this.header)
-//		this.constructTable(table);
-//	else {
-//		ColumnNode col = this.chooseNextColumn();
-//		col.cover();
-//		
-//		for (Node row = col.down ; row != col ; row = row.down) {
-//			this.answer.add(row);
-//			
-//			for (Node node = row.right ; node != row ; node = node.right)
-//				node.column.cover();
-//			
-//			table = this.search(k + 1, table);
-//			
-//			if (table != null)
-//				return table;
-//			
-//			row = this.answer.remove(this.answer.size() - 1);
-//			col = row.column;
-//			
-//			for (Node node = row.left ; node != row ; node = node.left)
-//				node.column.uncover();
-//		}
-//	}
-//	
-//	return table;
-//}
-//
-//private ColumnNode chooseNextColumn() {
-//	int min = Integer.MAX_VALUE;
-//	ColumnNode ret = null;
-//	
-//	for (ColumnNode col = (ColumnNode)this.header.right ; col != this.header ; col = (ColumnNode)col.right) {
-//		if (col.size < min) {
-//			min = col.size;
-//			ret = col;
-//		}
-//	}
-//	
-//	return ret;
-//}
-//
-//private Cell[][] constructTable(Cell[][] table) {
-//	
-//	for (Node node : this.answer) {
-//		Node rcNode = node;
-//		int min = Integer.parseInt(rcNode.column.name);
-//		
-//		for (Node temp = node.right ; temp != node ; temp = temp.right) {
-//			int val = Integer.parseInt(temp.column.name);
-//			
-//			if (val < min) {
-//				min = val;
-//				rcNode = temp;
-//			}
-//		}
-//		
-//		int answer1 = Integer.parseInt(rcNode.column.name);
-//		int answer2 = Integer.parseInt(rcNode.right.column.name);
-//		int row = answer1 / 16;
-//		int col = answer1 % 16;
-//		char value = this.values[(answer2 % 16)];
-//		table[row][col] = new ConcreteCell(value);
-//	}
-//	
-//	return table;
-//}
-//
-//private static class Node {
-//	Node up;
-//	Node down;
-//	Node left;
-//	Node right;
-//	ColumnNode column;
-//	
-//	Node() {
-//		this.left = this.right = this.up = this.down = this;
-//	}
-//	
-//	Node(ColumnNode column) {
-//		this();
-//		this.column = column;
-//	}
-//	
-//	Node hookDown(Node node) {
-//		node.down = this.down;
-//		node.down.up = node;
-//		node.up = this;
-//		this.down = node;
-//		
-//		return node;
-//	}
-//	
-//	Node hookRight(Node node) {
-//		node.right = this.right;
-//		node.right.left = node;
-//		node.left = this;
-//		this.right = node;
-//		
-//		return node;
-//	}
-//	
-//	void unlinkLeftRight() {
-//		this.left.right = this.right;
-//		this.right.left = this.left;
-//	}
-//	
-//	void relinkLeftRight() {
-//		this.left.right = this.right.left = this;
-//	}
-//	
-//	void unlinkUpDown() {
-//		this.up.down = this.down;
-//		this.down.up = this.up;
-//	}
-//	
-//	void relinkUpDown() {
-//		this.up.down = this.down.up = this;
-//	}
-//}
-//
-//private static class ColumnNode extends Node {
-//	int size;
-//	String name;
-//	
-//	ColumnNode(String name) {
-//		super();
-//		this.name = name;
-//		super.column = this;
-//	}
-//	
-//	void cover() {
-//		super.unlinkLeftRight();
-//		
-//		for (Node node1 = super.down ; node1 != this ; node1 = node1.down) {
-//			for (Node node2 = node1.right ; node2 != node1 ; node2 = node2.right) {
-//				node2.unlinkUpDown();
-//				node2.column.size--;
-//			}
-//		}
-//	}
-//	
-//	void uncover() {
-//		for (Node node1 = super.up ; node1 != this ; node1 = node1.up) {
-//			for (Node node2 = node1.left ; node2 != node1 ; node2 = node2.left) {
-//				node2.column.size++;
-//				node2.relinkUpDown();
-//			}
-//		}
-//		
-//		super.relinkLeftRight();
-//	}
-//}
+	@Override
+	public boolean hasUniqueSolution(Board board) {
+		this.board = board;
+		this.values = board.getLegalValues().getValues();
+		this.answer = new LinkedList<Node>();
+		
+		int dimensions = board.getDimensions();
+		boolean[][] matrix = this.createMatrix(board, dimensions);
+		this.placeInitialValues(matrix, dimensions);
+		this.header = this.createDoublyLinkedMatrix(matrix);
+		
+		int result = this.search(0, 0);
+		this.board = null;
+		this.values = null;
+		this.header = null;
+		this.answer = null;
+		
+		return result == 1;
+	}
+	
+	private boolean[][] createMatrix(Board board, int dimensions) {
+		boolean[][] matrix = new boolean[dimensions * dimensions * dimensions][4 * dimensions * dimensions];
+		
+		int hBase = 0;
+		hBase = this.checkCellConstraint(hBase, dimensions, matrix);
+		hBase = this.checkRowConstraint(hBase, dimensions, matrix);
+		hBase = this.checkColumnConstraint(hBase, dimensions, matrix);
+		this.checkBoxConstraint(hBase, dimensions, matrix);
+		
+		return matrix;
+	}
+	
+	private int checkCellConstraint(int hBase, int dimensions, boolean[][] matrix) {
+		for (int row = 0 ; row < dimensions ; row++) {
+			for (int col = 0 ; col < dimensions ; col++, hBase++) {
+				for (int valIndex = 0 ; valIndex < dimensions ; valIndex++) {
+					int index = this.getIndex(row, col, valIndex, dimensions);
+					matrix[index][hBase] = true;
+				}
+			}
+		}
+		
+		return hBase;
+	}
+	
+	private int checkRowConstraint(int hBase, int dimensions, boolean[][] matrix) {
+		for (int row = 0 ; row < dimensions ; row++) {
+			for (int valIndex = 0 ; valIndex < dimensions ; valIndex++, hBase++) {
+				for (int col = 0 ; col < dimensions ; col++) {
+					int index = this.getIndex(row, col, valIndex, dimensions);
+					matrix[index][hBase] = true;
+				}
+			}
+		}
+		
+		return hBase;
+	}
+	
+	private int checkColumnConstraint(int hBase, int dimensions, boolean[][] matrix) {
+		for (int col = 0 ; col < dimensions ; col++) {
+			for (int valIndex = 0 ; valIndex < dimensions ; valIndex++, hBase++) {
+				for (int row = 0 ; row < dimensions ; row++) {
+					int index = this.getIndex(row, col, valIndex, dimensions);
+					matrix[index][hBase] = true;
+				}
+			}
+		}
+		
+		return hBase;
+	}
+	
+	private void checkBoxConstraint(int hBase, int dimensions, boolean[][] matrix) {
+		int end = (int)Math.sqrt(dimensions);
+		
+		for (int row = 0 ; row < dimensions ; row += end) {
+			for (int col = 0 ; col < dimensions ; col += end) {
+				for (int valIndex = 0 ; valIndex < dimensions ; valIndex++, hBase++) {
+					for (int rowDelta = 0 ; rowDelta < end ; rowDelta++) {
+						for (int colDelta = 0 ; colDelta < end ; colDelta++) {
+							int index = this.getIndex(row + rowDelta, col + colDelta, valIndex, dimensions);
+							matrix[index][hBase] = true;
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	private int getIndex(int row, int col, int valIndex, int dimensions) {
+		return row * dimensions * dimensions + col * dimensions + valIndex;
+	}
+	
+	private void placeInitialValues(boolean[][] matrix, int dimensions) {
+		for (int i = 0 ; i < dimensions ; i++) {
+			for (int j = 0 ; j < dimensions ; j++) {
+				char value = board.getValueAt(i, j);
+				
+				if (value != '\u0000') {
+					for (int valIndex = 0 ; valIndex < dimensions ; valIndex++) {
+						if (value != this.values[valIndex])
+							Arrays.fill(matrix[this.getIndex(i, j, valIndex, dimensions)], false);
+					}
+				}
+			}
+		}
+	}
+	
+	private ColumnNode createDoublyLinkedMatrix(boolean[][] matrix) {
+		ColumnNode hNode = new ColumnNode("header");
+		List<ColumnNode> columnNodes = new ArrayList<ColumnNode>();
+		int cols = matrix[0].length;
+		
+		for (int i = 0 ; i < cols ; i++) {
+			ColumnNode node = new ColumnNode(String.valueOf(i));
+			columnNodes.add(node);
+			hNode = (ColumnNode)hNode.hookRight(node);
+		}
+		
+		hNode = hNode.right.column;
+		
+		for (boolean[] row : matrix) {
+			Node prev = null;
+			
+			for (int col = 0 ; col < cols ; col++) {
+				if (row[col]) {
+					ColumnNode colNode = columnNodes.get(col);
+					Node newNode = new Node(colNode);
+					
+					if (prev == null)
+						prev = newNode;
+					
+					colNode.up.hookDown(newNode);
+					prev = prev.hookRight(newNode);
+					colNode.size++;
+				}
+			}
+		}
+		
+		hNode.size = cols;
+		
+		return hNode;
+	}
+	
+	private int search(int k, int count) {
+		if (this.header.right == this.header)
+			count++;
+		else {
+			ColumnNode col = this.chooseNextColumn();
+			col.cover();
+			
+			for (Node row = col.down ; row != col ; row = row.down) {
+				this.answer.add(row);
+				
+				for (Node node = row.right ; node != row ; node = node.right)
+					node.column.cover();
+				
+				count = this.search(k + 1, count);
+				
+				row = this.answer.remove(this.answer.size() - 1);
+				col = row.column;
+				
+				for (Node node = row.left ; node != row ; node = node.left)
+					node.column.uncover();
+			}
+		}
+		
+		return count;
+	}
+	
+	private ColumnNode chooseNextColumn() {
+		int min = Integer.MAX_VALUE;
+		ColumnNode ret = null;
+		
+		for (ColumnNode col = (ColumnNode)this.header.right ; col != this.header ; col = (ColumnNode)col.right) {
+			if (col.size < min) {
+				min = col.size;
+				ret = col;
+			}
+		}
+		
+		return ret;
+	}
+	
+	private static class Node {
+		Node up;
+		Node down;
+		Node left;
+		Node right;
+		ColumnNode column;
+		
+		Node() {
+			this.up = this;
+			this.down = this;
+			this.left = this;
+			this.right = this;
+		}
+		
+		Node(ColumnNode columnNode) {
+			this();
+			this.column = columnNode;
+		}
+		
+		Node hookDown(Node node) {
+			node.down = this.down;
+			node.down.up = node;
+			node.up = this;
+			this.down = node;
+			
+			return node;
+		}
+		
+		Node hookRight(Node node) {
+			node.right = this.right;
+			node.right.left = node;
+			node.left = this;
+			this.right = node;
+			
+			return node;
+		}
+		
+		void unlinkLeftRight() {
+			this.left.right = this.right;
+			this.right.left = this.left;
+		}
+		
+		void relinkLeftRight() {
+			this.left.right = this.right.left = this;
+		}
+		
+		void unlinkUpDown() {
+			this.up.down = this.down;
+			this.down.up = this.up;
+		}
+		
+		void relinkUpDown() {
+			this.up.down = this.down.up = this;
+		}
+	}
+	
+	private static class ColumnNode extends Node {
+		int size = 0;
+		String name;
+		
+		ColumnNode(String name) {
+			super();
+			this.name = name;
+			super.column = this;
+		}
+		
+		void cover() {
+			super.unlinkLeftRight();
+			
+			for (Node node1 = super.down ; node1 != this ; node1 = node1.down) {
+				for (Node node2 = node1.right ; node2 != node1 ; node2 = node2.right) {
+					node2.unlinkUpDown();
+					node2.column.size--;
+				}
+			}
+		}
+		
+		void uncover() {
+			for (Node node1 = super.up ; node1 != this ; node1 = node1.up) {
+				for (Node node2 = node1.left ; node2 != node1 ; node2 = node2.left) {
+					node2.column.size++;
+					node2.relinkUpDown();
+				}
+			}
+			
+			super.relinkLeftRight();
+		}
+	}
 }
